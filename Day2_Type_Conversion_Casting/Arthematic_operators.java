db1ad7daf3bbc3d36ad78c9456f97700599acf28
Muhammad Haseeb <mh2951813@gmail.com>
2026-03-30 21:31:22 +0500

    Add files via upload

---
+++ Day2_Type_Conversion_Casting/Arthematic_operators.java
@@ -0,0 +1,24 @@
+class Arthematic_operators{
+  public static void main(String[]arg){
+ int a,b,c,d,f,g,h;
+ a=10;
+ b=20;
+ c=a+b;
+ d=a-b;
+ f=a/b;
+ g=a%b;
+ h=a*b;
+ System.out.println(c);
+ System.out.println(d);
+ System.out.println(f);
+ System.out.println(g);
+ System.out.println(h);
+ 
+ int num=10;
+ int result = ++num;  //pre increment 
+ int result1 = num++;  // post increment
+ System.out.println(result);
+ //
+ System.out.println(result1);
+  }
+}
---
+++ Day2_Type_Conversion_Casting/Comparison_Operators.java
@@ -0,0 +1,27 @@
+class Comparison_Operators{
+  public static void main (String[]arg){
+ 
+  // WE Have many comparison operators such as >,<,==,!=,<=,>=
+  // when we will use these comparison operators then it will return the boolean value
+  int a=10;
+  int b=20;
+  boolean result;
+  //result = a < b
+  result = a > b;
+  boolean result1 = a <= b;  // or operators here means  two of them only one can true it lead to ture
+  boolean result2 = a >= b;
+  boolean result3 = a == b;
+  System.out.println(result);
+  System.out.println(result1);
+  System.out.println(result2);
+  System.out.println(result3);
+
+double a1=10.1;
+  double b1=10.2;
+boolean result7 = a1 >= b1;
+   System.out.println(result7);
+
+
+
+  }
+}
