9d3af57989d98d20b50140a0026a6680ca2ed99f
Muhammad Haseeb <mh2951813@gmail.com>
2026-03-31 09:54:14 +0500

    Add files via upload

---
+++ Day3_Loops_Switch_Class&objects/While_Loop.java
@@ -0,0 +1,36 @@
+class While_Loop {
+  public static void main(String[]arg){
+  // while loop is used along with increment and decrement 
+  int i=8;
+ // while(i<=10){
+   // System.out.println(3 + " * " + i + " = " + i*3);
+    //i++;
+  //}
+   
+//  while (i<8){  // right trianlge print
+//   int j=1;
+//   while (j<=i){
+//     System.out.print("*" + "  ");
+//     j++;
+    
+//   }
+//   System.out.println();
+//   i++;
+
+//  }
+while (i>1){   // oposite triangle print
+ int j=1;
+
+ while(j<=i){
+  System.out.print("*" );
+  j++;
+ }
+
+System.out.println();
+i--;
+}
+
+
+
+  }
+}
