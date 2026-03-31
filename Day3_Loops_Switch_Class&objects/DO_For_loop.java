229e107803128ec0f1740f6cc25333d4df8cfd3a
Muhammad Haseeb <mh2951813@gmail.com>
2026-03-31 12:37:08 +0500

    Add files via upload

---
+++ Day3_Loops_Switch_Class&objects/DO_For_loop.java
@@ -0,0 +1,45 @@
+public class DO_For_loop {
+  public static void main(String[]arg){
+//  int i =1;
+//  do{
+//  System.out.println("Hello ");
+//   i++;
+//  }
+//  while(i<3);
+
+// ATM MACHINE PRATICE WITH DO WHILE LOOPS
+// int choice=1;
+// do{
+//     System.out.println("1 ---- Check Balance");
+//     System.out.println("2 ---- Deposit Balance");
+//     System.out.println("3 ---- Widraw Balance");
+//     System.out.println("4 ---- exit");
+//   if (choice==1){
+//     System.out.print("Balance is 100");
+//   }
+//   else if (choice==2){
+//     System.out.print("Deposit is 100");
+//   }
+//   else if (choice==3){
+//     System.out.print("Widraw 100 Successfully");
+//   }
+//   else if (choice==4){
+//     System.out.print("Returning");
+//   }
+// choice++;
+
+
+
+// }
+// while(choice<4);
+
+
+   // For Loops Even Number find
+   for(int i=0;i<40;i++){
+    if(i%2==0){
+      System.out.println(i);
+    }
+   }
+
+  }
+}
